e3b1e0746ac549c876d1c33247009f891e77cdd7
rtmigo <art.galkin@gmail.com>
2021-03-10 16:45:06 +0300

    ci

--- sample_project/test_pkg_as_module.sh
+++ sample_project/test_pkg_as_module.sh
@@ -2,5 +2,11 @@
 set -e && source "scripts/pyrel.sh"
 
 pyrel_test_begin # check it builds
-python3 -c "import greeter; greeter.say_hi()"
+
+# check we can import it
+python3 -c "import greeter"
+
+# or run a function and save the output to a file
+python3 -c "import greeter; greeter.say_hi()" > ~/output.txt
+
 pyrel_test_end # cleanup
